d65cb3a171985ffaef5a6a3770c4fbc3ec4fe512
faustin12-g <nshimiefaustinpeace@gmail.com>
2026-02-11 15:06:21 +0200

    add react tailwind configuration

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-amber-900 font-bold text-5xl text-center'>The react demo</h1>
+    </div>
+  )
+}
+
+export default App
